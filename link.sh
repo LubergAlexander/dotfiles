4553a5c10c3e9389f64b50c63e6855f3e32ca187
Alexander Luberg <alex@luberg.me>
2013-07-27 11:57:37 +0300

    Remove before ln

--- link.sh
+++ link.sh
@@ -5,5 +5,6 @@ rm $HOME/.tmux.conf
 ln .vimrc $HOME/.vimrc
 ln .zshrc $HOME/.zshrc
 ln .tmux.conf $HOME/.tmux.conf
+sudo rm /usr/local/bin/copy-to-clipboard
 sudo ln copy-to-clipboard /usr/local/bin/copy-to-clipboard
 sudo chmod a+x /usr/local/bin/copy-to-clipboard
