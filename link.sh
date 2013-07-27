0e420e35554e457dbb73c189d5fb74adf40dbc7f
Alexander Luberg <alex@luberg.me>
2013-07-27 11:30:11 +0300

    Since tmux 1.8 an option copy-pipe was introduced. Added config to do working copypaste for both ubuntu and mac

--- link.sh
+++ link.sh
@@ -5,3 +5,5 @@ rm $HOME/.tmux.conf
 ln .vimrc $HOME/.vimrc
 ln .zshrc $HOME/.zshrc
 ln .tmux.conf $HOME/.tmux.conf
+sudo ln copy-to-clipboard /usr/local/bin/copy-to-clipboard
+sudo chmod a+x /usr/local/bin/copy-to-clipboard
