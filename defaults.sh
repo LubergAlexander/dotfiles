55367caea75ee3a9db2a8f7931f6758c2758aae8
Alexander Luberg <alex@luberg.me>
2024-12-20 12:41:36 -0800

    macos defaults

--- defaults.sh
+++ defaults.sh
@@ -1,3 +1,149 @@
-# Disable press-and-hold for keys in favor of key repeat
+#!/usr/bin/env zsh
+
+# Close any open System Preferences to prevent overriding changes
+osascript -e 'tell application "System Preferences" to quit'
+
+# Ask for administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until script has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+###############################################################################
+# Key Repeat & Keyboard                                                         #
+###############################################################################
+
+# Enable fast key repeat - essential for Vim
+defaults write NSGlobalDomain KeyRepeat -int 1
+defaults write NSGlobalDomain InitialKeyRepeat -int 10
 defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 
+###############################################################################
+# Dock                                                                         #
+###############################################################################
+
+# Position dock on the left
+defaults write com.apple.dock orientation -string "left"
+
+# Set dock to auto-hide
+defaults write com.apple.dock autohide -bool true
+
+# Remove auto-hide delay
+defaults write com.apple.dock autohide-delay -float 0
+defaults write com.apple.dock autohide-time-modifier -float 0.3
+
+# Make icons smaller
+defaults write com.apple.dock tilesize -int 48
+
+# Don't show recent applications
+defaults write com.apple.dock show-recents -bool false
+
+# Minimize windows into application icon
+defaults write com.apple.dock minimize-to-application -bool true
+
+###############################################################################
+# Finder                                                                       #
+###############################################################################
+
+# Show hidden files by default
+defaults write com.apple.finder AppleShowAllFiles -bool true
+
+# Show all filename extensions
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+# Disable the warning when changing a file extension
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+
+# Show path bar
+defaults write com.apple.finder ShowPathbar -bool true
+
+# Set search scope to current folder by default
+defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+
+# Keep folders on top when sorting by name
+defaults write com.apple.finder _FXSortFoldersFirst -bool true
+
+###############################################################################
+# General UI/UX                                                                #
+###############################################################################
+
+# Disable the "Are you sure you want to open this application?" dialog
+defaults write com.apple.LaunchServices LSQuarantine -bool false
+
+# Disable window animations
+defaults write NSGlobalDomain NSAutomaticWindowAnimationsEnabled -bool false
+
+# Accelerate window resizing
+defaults write NSGlobalDomain NSWindowResizeTime -float 0.001
+
+# Speed up Mission Control animations
+defaults write com.apple.dock expose-animation-duration -float 0.1
+
+# Disable disk image verification
+defaults write com.apple.frameworks.diskimages skip-verify -bool true
+defaults write com.apple.frameworks.diskimages skip-verify-locked -bool true
+defaults write com.apple.frameworks.diskimages skip-verify-remote -bool true
+
+# Enable spring loading for directories
+defaults write NSGlobalDomain com.apple.springing.enabled -bool true
+defaults write NSGlobalDomain com.apple.springing.delay -float 0
+
+###############################################################################
+# Screen                                                                       #
+###############################################################################
+
+# Require password immediately after sleep or screen saver
+defaults write com.apple.screensaver askForPassword -int 1
+defaults write com.apple.screensaver askForPasswordDelay -int 0
+
+# Save screenshots to Downloads folder
+defaults write com.apple.screencapture location -string "${HOME}/Screenshots"
+
+# Save screenshots in PNG format
+defaults write com.apple.screencapture type -string "png"
+
+###############################################################################
+# Activity Monitor                                                             #
+###############################################################################
+
+# Show the main window when launching Activity Monitor
+defaults write com.apple.ActivityMonitor OpenMainWindow -bool true
+
+# Show all processes in Activity Monitor
+defaults write com.apple.ActivityMonitor ShowCategory -int 0
+
+###############################################################################
+# Text Editing                                                                 #
+###############################################################################
+
+# Disable smart quotes and dashes
+defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
+defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
+
+# Disable auto-correct
+defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+
+###############################################################################
+# Trackpad                                                                     #
+###############################################################################
+
+# Enable tap to click
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true
+
+###############################################################################
+# Menu Bar                                                                     #
+###############################################################################
+
+# Show battery percentage
+defaults write com.apple.menuextra.battery ShowPercent -bool true
+
+###############################################################################
+# Kill affected applications                                                   #
+###############################################################################
+
+for app in "Activity Monitor" "Dock" "Finder" "SystemUIServer"; do
+    killall "${app}" &> /dev/null
+done
+
+echo "Done. Note that some of these changes require a logout/restart to take effect."
